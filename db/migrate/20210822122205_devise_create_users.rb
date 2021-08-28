936239e9fc5281516c5d7b87e772f64f4925ca08
hiiro777 <sleeping.garden@icloud.com>
2021-08-28 14:42:56 +0900

    Revert "ユーザー新規登録"
    This reverts commit 3ec992e23e547f962d7eb79d54a9e483f1072f16.

--- db/migrate/20210822122205_devise_create_users.rb
+++ db/migrate/20210822122205_devise_create_users.rb
@@ -4,13 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :nickname,           null: false
-      t.string :last_name,          null: false
-      t.string :first_name,         null: false
-      t.string :last_furigana,       null: false
-      t.string :first_furigana,      null: false
-      t.date   :birthday,           null: false
-      t.string :email,              null: false, unique: true, default: ""
+      t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
